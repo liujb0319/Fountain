4d7ddad4386259481e6b9c42cf47439bcaf6e7cf
Dave Matney <david.matney@extendhealth.com>
2016-09-22 16:04:30 -0600

    Cleaned up cs file using statements so things would build

--- Fountain/Controls/GradientBox.cs
+++ Fountain/Controls/GradientBox.cs
@@ -14,15 +14,9 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-using System;
-using System.Collections.Generic;
 using System.ComponentModel;
 using System.Drawing;
 using System.Drawing.Drawing2D;
-using System.Data;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 using LlewellynMedia;
--- Fountain/Forms/GeneratorDialog.cs
+++ Fountain/Forms/GeneratorDialog.cs
@@ -15,19 +15,8 @@
 */
 
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Reflection;
-
 using LlewellynScripting;
-using LlewellynMedia;
-
 using Fountain.Media;
 
 namespace Fountain.Forms
